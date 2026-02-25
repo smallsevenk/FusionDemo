ad4490771ff78d86455ab806a365b9decb03312e
Andy <smallsevenk@vip.qq.com>
2026-02-25 10:18:27 +0800

    ♻️ update:.ios/.android

---
+++ fusion_flutter_module/.ios/Flutter/FlutterPluginRegistrant/Classes/GeneratedPluginRegistrant.m
@@ -0,0 +1,21 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<fusion/FusionPlugin.h>)
+#import <fusion/FusionPlugin.h>
+#else
+@import fusion;
+#endif
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [FusionPlugin registerWithRegistrar:[registry registrarForPlugin:@"FusionPlugin"]];
+}
+
+@end
